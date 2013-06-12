6a8560593d99ba9b3702f1f9353c9fdaca81e808
Evan Warfel <e.warfel@gmail.com>
2013-06-12 14:02:43 -0700

    Dual Order Generation Finalized

--- Dual_order_generator.py
+++ Dual_order_generator.py
@@ -25,14 +25,89 @@ def dual_tester(listnumb,n):#when its around 10%, I'll copy it into catgameconst
         total = total + count
     return total/len(listnumb)
 
-def Dual_order_gen_alt(n)
-    FOR SOME LOOOP
-        a = range(0,16)
+def Dual_order_gen_alt1(m):# for n-back ==1, m is blocks of 30, so m ==10 would yield a list 300 items long
+    master_ls=[]
+    for k in range(0,m):
+        order_ls = []
+        a = range(0,15)
+        random.shuffle(a)
         b = a[0:3]
         aa = a[3:]
-        c = random.shuffle(a)
-        d = random.shuffle(a)
-        e = c+d
+        bb = a[3:]
+        random.shuffle(aa)
+        random.shuffle(bb)
+        z = bb+aa
+        
+        for i in range(0,3):
+            d = random.randint(0,8)
+            
+            for j in range(0,8):
+                if j == d:
+                    numb =  [b[i],b[i]]
+                else:
+                    index = j*(i+1)
+                    numb = z[index]
+                    
+                order_ls.append(numb)
+              
+                
+        master_ls.append(order_ls)
+        
+    return flatten(master_ls)
+
+def Dual_order_gen_alt2(m):# for n-back ==2, m is blocks of 30, so m ==10 would yield a list 300 items long
+    master_ls=[]
+    flag = False
+    altflag = False
+    for k in range(0,m):
+        print 'k',k
+        order_ls = []
+        a = range(0,15)
+        random.shuffle(a)
+        b = a[0:3]
+        aa = a[3:]
+        bb = a[3:]
+        random.shuffle(aa)
+        random.shuffle(bb)
+        z = bb+aa
+        f = [0,1,2,3,4,5,6,7,8]
+        
+        for i in range(0,3):
+            d = random.sample(f,3)
+            for j in range(0,8):
+                index = j*(i+1)
+
+                
+                if j == d[i]:
+                    numb =  [b[i],z[index],b[i]]
+                    flag = True
+              #  elif flag:
+              #      flag = False
+              #      altflag = True
+              #      pass
+              #  elif altflag:
+              #      altflag= False
+              #      pass
+                else:
+
+                    numb = z[index]
+                    
+                order_ls.append(numb)
+              
+                
+        master_ls.append(order_ls)
+        
+    return flatten(master_ls)
+                
+
+def flatten(l):#list flattener
+  out = []
+  for item in l:
+    if isinstance(item, (list, tuple)):
+      out.extend(flatten(item))
+    else:
+      out.append(item)
+  return out        
         
                 
             
